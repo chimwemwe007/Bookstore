9f262e8df31638a2d2f9c37acba9d3bc39fdc8be
Chimwemwe Mkandawire <chimwe.wire@gmail.com>
2022-08-17 22:43:58 +0200

    Add reducer to book category function

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,9 +1,8 @@
 const CHECK = 'bookstore/categories/CHECK';
 
-// Reducer
+//Reducer
 export default function categoriesReducer(state = [], action = {}) {
   switch (action.type) {
-    // do reducer stuff
     case CHECK:
       return 'Under Constuction';
     default:
