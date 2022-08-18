969e64509ee49d84f3681127ce7ceec6261e8714
Chimwemwe Mkandawire <chimwe.wire@gmail.com>
2022-08-18 09:52:11 +0200

    Add react hooks and immutability

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,17 +1,32 @@
+import { v4 as rid } from 'uuid';
 // Actions
 const LOAD = 'bookstore/books/LOAD_BOOK';
 const ADD = 'bookstore/books/ADD_BOOK';
 const REMOVE = 'bookstore/books/REMOVE_BOOK';
 
+const books = [
+  { id: rid(), name: 'In Search of Lost Time', author: 'Marcel Proust' },
+  { id: rid(), name: 'One Hundred Years of Solitude', author: ' Gabriel Garcia Marquez' }];
 // Reducer
-export default function booksReducer(state = [], action = {}) {
+export default function booksReducer(state = books, action = {}) {
+  function remove(sourceList, value) {
+    const index = sourceList.indexOf(value);
+    if (index >= 0 && index < sourceList.length) {
+      return [
+        ...sourceList.slice(0, index),
+        ...sourceList.slice(index + 1),
+      ];
+    }
+    return sourceList;
+  }
+  const { value } = action;
   switch (action.type) {
     case LOAD:
-      return state;
+      return [...state];
     case ADD:
-      return state.push(action.value);
+      return [...state, value];
     case REMOVE:
-      return state.filter((m) => m.id !== action.value);
+      return remove(state, state.filter((i) => i.id === value)[0]);
     default:
       return state;
   }
@@ -22,10 +37,10 @@ export function loadBooks() {
   return { type: LOAD };
 }
 
-export function addBook(book) {
-  return { type: ADD, value: book };
+export function addBook(title, author) {
+  return { type: ADD, value: { id: rid(), name: title, author } };
 }
 
 export function removeBook(book) {
-  return { type: REMOVE, book };
+  return { type: REMOVE, value: book };
 }
