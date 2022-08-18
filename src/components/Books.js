2b01cdde88be1697ae4b05c991b61c1de364c68a
Chimwemwe Mkandawire <chimwe.wire@gmail.com>
2022-08-18 09:38:59 +0200

    Add redux hooks and immutability to project

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,27 +1,17 @@
 import React from 'react';
+import { useSelector, shallowEqual } from 'react-redux';
 import AddBook from './AddBook';
 import Book from './Book';
 
-class Books extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      Books: [
-        { id: 1, name: 'In Search of Lost Time', author: 'Marcel Proust' },
-        { id: 2, name: 'One Hundred Years of Solitude', author: ' Gabriel Garcia Marquez' }],
-    };
-  }
+function Books() {
+  const Books = useSelector((state) => state.books, shallowEqual);
+  return (
+    <>
+      {Books.map((m) => <Book key={m.id} id={m.id} name={m.name} author={m.author} />)}
+      <AddBook />
+    </>
 
-  render() {
-    const { Books } = this.state;
-    return (
-      <>
-        {Books.map((m) => <Book key={m.id} name={m.name} author={m.author} />)}
-        <AddBook />
-      </>
-
-    );
-  }
+  );
 }
 
 export default Books;
