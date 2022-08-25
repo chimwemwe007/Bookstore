d5e6c6491aa7a11823787c49079f58f126fa1512
Chimwemwe Mkandawire <chimwe.wire@gmail.com>
2022-08-25 10:39:21 +0200

    Add middleware to redux store

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './redux/configureStore';
+import { loadBooks } from './redux/books/books';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+store.dispatch(loadBooks());
 root.render(
   <Provider store={store}>
     <BrowserRouter>
@@ -17,7 +19,7 @@ root.render(
 
 );
 
-// If you want to start measuring performance in your app, pass a function.
+// If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
