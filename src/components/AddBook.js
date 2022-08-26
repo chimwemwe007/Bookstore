e2762b7705b210026a4018787baeb413c83865be
Chimwemwe Mkandawire <chimwe.wire@gmail.com>
2022-08-26 09:25:43 +0200

    Add styling

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,12 +1,38 @@
-const AddBook = () => (
-  <section>
-    <h2 className="form-title">ADD NEW BOOK</h2>
-    <form className="add-form">
-      <input className="input title-input" placeholder="Book title" required="" value="" />
-      <input className="input author-input" placeholder="Book Author" required="" value="" />
-      <button className="primary-button-big" type="submit">ADD BOOK</button>
-    </form>
-  </section>
-);
+import { useDispatch } from 'react-redux';
+import { v4 as rid } from 'uuid';
+import { addBook } from '../redux/books/books';
+
+const AddBook = () => {
+  const dispatch = useDispatch();
+  const performSubmit = (e) => {
+    e.preventDefault();
+    const title = e.target[0].value;
+    const author = e.target[1].value;
+    const category = e.target[2].options[e.target[2].selectedIndex].text;
+    const iid = rid();
+    const book = {
+      id: iid,
+      name: title,
+      author,
+      category,
+    };
+    dispatch(addBook(book));
+  };
+  return (
+    <section>
+      <h2 className="form-title">ADD NEW BOOK</h2>
+      <form className="add-form" onSubmit={performSubmit}>
+        <input onChange={() => {}} className="input title-input" placeholder="Book title" required="" />
+        <input onChange={() => {}} className="input author-input" placeholder="Book Author" required="" />
+        <select className="input category-input">
+          <option value="Action">Action</option>
+          <option value="Science Fiction">Science Fiction</option>
+          <option value="Economy">Economy</option>
+        </select>
+        <button className="primary-button-big" type="submit">ADD BOOK</button>
+      </form>
+    </section>
+  );
+};
 
 export default AddBook;
